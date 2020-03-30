9caf18bb597143558427383f46d0d4a91ee3b18c
Viennet <viennet.t@gmail.com>
2020-03-30 18:35:00 +0200

    console.log pour la page

--- screenSERP.js
+++ screenSERP.js
@@ -10,15 +10,23 @@ const puppeteer = require('puppeteer');
   console.log('Mots clés :' + keywords);
   for (var i = 0; i < keywords.length; i++) {
 
-    console.log(keywords[i] + ' en cours');
+    console.log(keywords[i] + ' : Screenshot page 1');
     await page.goto('https://google.com/search?q=' + keywords[i]);
     await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '.png', fullPage: true });
+
+    console.log(keywords[i] + ' : Screenshot page 2');
     await page.goto('https://google.com/search?q='  + keywords[i] + '&start=10');
     await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_2.png', fullPage: true });
+
+    console.log(keywords[i] + ' : Screenshot page 3');
     await page.goto('https://google.com/search?q='  + keywords[i] + '&start=20');
     await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_3.png', fullPage: true });
+
+    console.log(keywords[i] + ' : Screenshot page 4');
     await page.goto('https://google.com/search?q='  + keywords[i] + '&start=30');
     await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_4.png', fullPage: true });
+
+    console.log(keywords[i] + ' : Screenshot page 5');
     await page.goto('https://google.com/search?q='  + keywords[i] + '&start=40');
     await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_5.png', fullPage: true });
   }
