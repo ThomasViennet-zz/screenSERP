fbb4ff07c6616f050fdc5aade43d57c3cf051b15
Viennet <viennet.t@gmail.com>
2020-03-30 18:18:07 +0200

    commit initial

---
+++ screenSERP.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  console.log('Initialisation');
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+
+  var keywords = ['evoliz', 'evoliz+avis', 'evoliz+lol', 'alternative+evoliz', 'concurrents+evoliz', 'comparateurs+evoliz'];
+
+  console.log('Mots clés :' + keywords);
+  for (var i = 0; i < keywords.length; i++) {
+
+    console.log(keywords[i] + ' en cours');
+    await page.goto('https://google.com/search?q=' + keywords[i]);
+    await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '.png', fullPage: true });
+    await page.goto('https://google.com/search?q='  + keywords[i] + '&start=10');
+    await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_2.png', fullPage: true });
+    await page.goto('https://google.com/search?q='  + keywords[i] + '&start=20');
+    await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_3.png', fullPage: true });
+    await page.goto('https://google.com/search?q='  + keywords[i] + '&start=30');
+    await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_4.png', fullPage: true });
+    await page.goto('https://google.com/search?q='  + keywords[i] + '&start=40');
+    await page.screenshot({path: 'screens/' + Date.now() + keywords[i] + '_page_5.png', fullPage: true });
+  }
+
+  console.log('Fin');
+  await browser.close();
+})();
